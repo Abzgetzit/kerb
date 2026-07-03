d7badd1f48c7308ce322e01259e64736a27060d2
Abzgetzit <hajatabz@gmail.com>
2026-07-03 22:45:13 +0100

    Add account password metadata

---
+++ app/account/password/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Change Password | Kerb Car",
+  description: "Change the password for your Kerb Car account.",
+};
+
+export default function AccountPasswordLayout({ children }) {
+  return children;
+}
