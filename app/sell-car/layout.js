597c95098c9813ad03e142592c6353ac9432a23e
Abzgetzit <hajatabz@gmail.com>
2026-07-03 22:30:23 +0100

    Tighten sell car metadata

--- app/sell-car/layout.js
+++ app/sell-car/layout.js
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Sell Your Car Online for Free | Kerb Car",
+  title: "Sell Your Car Online | Kerb Car",
   description:
     "Create a free car listing on Kerb Car with photos, vehicle details and buyer enquiries in one simple dashboard.",
 };
