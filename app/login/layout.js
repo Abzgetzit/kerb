8e09f6771eeb8e8024428004133cebd72975cc29
Abzgetzit <hajatabz@gmail.com>
2026-07-03 20:04:26 +0100

    Add login page metadata layout

---
+++ app/login/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Sign in to Kerb Car | Kerb Car",
+  description:
+    "Sign in to your Kerb Car account to manage listings, saved cars, enquiries and seller tools.",
+};
+
+export default function LoginLayout({ children }) {
+  return children;
+}
