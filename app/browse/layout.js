cf98226cbfa19e7499b5de39488d42c4672b3421
Abzgetzit <hajatabz@gmail.com>
2026-07-03 20:04:14 +0100

    Add browse page metadata layout

---
+++ app/browse/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Browse Used Cars for Sale | Kerb Car",
+  description:
+    "Search used cars for sale in the UK on Kerb Car. Filter by make, model, price, mileage, fuel type and more.",
+};
+
+export default function BrowseLayout({ children }) {
+  return children;
+}
