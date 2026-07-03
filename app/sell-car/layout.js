41967399fe12018ee7af1eddcd00c277fe18b490
Abzgetzit <hajatabz@gmail.com>
2026-07-03 20:04:21 +0100

    Add sell car page metadata layout

---
+++ app/sell-car/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Sell Your Car Online for Free | Kerb Car",
+  description:
+    "Create a free car listing on Kerb Car with photos, vehicle details and buyer enquiries in one simple dashboard.",
+};
+
+export default function SellCarLayout({ children }) {
+  return children;
+}
