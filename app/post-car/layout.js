9a5034eeebc1202d2140a900638af4dffba54023
Abzgetzit <hajatabz@gmail.com>
2026-07-03 22:27:57 +0100

    Add post car metadata

---
+++ app/post-car/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Sell Your Car Online | Kerb Car",
+  description:
+    "Create a car listing on Kerb Car with photos, vehicle details and buyer enquiries in one simple dashboard.",
+};
+
+export default function PostCarLayout({ children }) {
+  return children;
+}
