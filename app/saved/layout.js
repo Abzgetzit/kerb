5758ab65463b2ced945e23af647fbfc27b1a7b11
Abzgetzit <hajatabz@gmail.com>
2026-07-03 22:44:42 +0100

    Add saved cars metadata

---
+++ app/saved/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Saved Cars | Kerb Car",
+  description:
+    "Sign in to Kerb Car to view and compare your saved cars.",
+};
+
+export default function SavedLayout({ children }) {
+  return children;
+}
