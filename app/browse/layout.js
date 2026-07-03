06949bbf9d8224e926a0234a277a1353088d5478
Abzgetzit <hajatabz@gmail.com>
2026-07-03 22:30:31 +0100

    Tighten browse metadata layout

--- app/browse/layout.js
+++ app/browse/layout.js
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Browse Used Cars for Sale | Kerb Car",
+  title: "Browse Used Cars for Sale in the UK | Kerb Car",
   description:
     "Search used cars for sale in the UK on Kerb Car. Filter by make, model, price, mileage, fuel type and more.",
 };
